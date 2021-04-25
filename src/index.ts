49e531588ea48a362142e3e571b7d295f9f4549d
GrosfeldEzekiel <grosfeldezekiel@gmail.com>
2021-04-25 19:49:08 -0300

    Updating Package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Errors
 export * from "./errors/bad-request-error";
 export * from "./errors/custom-error";
 export * from "./errors/database-connection-error";
@@ -5,14 +6,17 @@ export * from "./errors/not-found-error";
 export * from "./errors/request-validation-error";
 export * from "./errors/unauthorized-error";
 
+// Middlewares
 export * from "./middlewares/error-handler";
 export * from "./middlewares/require-auth";
 export * from "./middlewares/validate-request";
 
+// Events
 export * from "./events/base-listener";
 export * from "./events/base-publisher";
 export * from "./events/subjects";
 export * from "./events/services-events/tickets";
 export * from "./events/services-events/orders";
 
+// Types
 export * from "./events/types/order-status";
